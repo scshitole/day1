8ec8ca2dff8bbd67cf7d84b06ebbe913dc79b354
Sanjay shitole <scshitole@gmail.com>
2024-05-10 17:30:26 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,1 +1,12 @@
-
+variable "address" {
+  description = "provide some prefix for deployment"
+}
+variable "port" {
+  description = "provide some prefix for deployment"
+}
+variable "username" {
+  description = "provide some prefix for deployment"
+}  
+variable "password" {
+  description = "provide some prefix for deployment"
+}
